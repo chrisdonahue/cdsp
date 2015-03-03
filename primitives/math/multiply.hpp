689cd1f666534b0dad65ee192939953551ffb204
Chris Donahue <christopherdonahue@gmail.com>
2015-03-03 15:52:26 -0800

    added all necessary primitives for project

---
+++ primitives/math/multiply.hpp
@@ -0,0 +1,18 @@
+/*
+  ==============================================================================
+
+    multiply.h
+    Created: 3 Mar 2015 3:48:13pm
+    Author:  Chris
+
+  ==============================================================================
+*/
+
+#ifndef MULTIPLY_H_INCLUDED
+#define MULTIPLY_H_INCLUDED
+
+
+
+
+
+#endif  // MULTIPLY_H_INCLUDED
