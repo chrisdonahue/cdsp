e6af8b2d074c27f09002ea1013fdf5e20cd8ab35
Chris Donahue <christopherdonahue@gmail.com>
2015-03-04 04:24:04 -0800

    work on adsr

--- primitives/envelopes/adsr.hpp
+++ primitives/envelopes/adsr.hpp
@@ -1,18 +1,28 @@
-/*
-  ==============================================================================
+#ifndef CDSP_PRIMITIVES_ENVELOPES_ADSR
+#define CDSP_PRIMITIVES_ENVELOPES_ADSR
 
-    adsr.h
-    Created: 3 Mar 2015 3:51:54pm
-    Author:  Chris
+namespace cdsp { namespace primitives { namespace envelopes {
+	class adsr {
+	public:
+		adsr();
+		adsr(types::sample _atk, types::sample _dcy, types::sample _sus, types::sample _rel);
 
-  ==============================================================================
-*/
+		void perform(sample_buffer& buffer, types::disc_32_u block_size_leq, types::channel offset_channel = 0, types::disc_32_u offset_sample = 0);
 
-#ifndef ADSR_H_INCLUDED
-#define ADSR_H_INCLUDED
+		void atk_set(types::sample _atk);
+		void dcy_set(types::sample _dcy);
+		void sus_set(types::sample _sus);
+		void rel_set(types::sample _rel);
 
+		void release();
+		void reset();
 
+	private:
+		types::sample atk;
+		types::sample dcy;
+		types::sample sus;
+		types::sample rel;
+	};
+}}}
 
-
-
-#endif  // ADSR_H_INCLUDED
+#endif
