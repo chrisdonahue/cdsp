59e1a3f1af03b0f94da804b39380e676a6c66b8c
Chris Donahue <christopherdonahue@gmail.com>
2015-03-15 17:53:51 -0700

    working on linear envleope

--- src/primitives/envelopes/envelopes_base.hpp
+++ src/primitives/envelopes/envelopes_base.hpp
@@ -7,18 +7,47 @@
 #include "../primitives_base.hpp"
 
 namespace cdsp { namespace primitives { namespace envelopes {
+	template <types::disc_32_u points_num>
 	class base : public primitives::base {
 	protected:
-		typedef std::pair<types::cont_64, types::sample> point;
+		typedef std::pair<types::time, types::sample> point;
 
 	public:
-		base();
-		base(types::sample _value_initial);
-		base(types::sample _value_initial, std::vector<point>& _points);
+		base() :
+			primitives::base(),
+			value(values::sample_silence),
+			value_initial(values::sample_silence),
+			points(points_num),
+			points_index_current(0)
+		{
+			channels_input_num_set(0);
+			channels_output_num_set(1);
+		};
+
+		base(types::sample _value_initial) :
+			primitives::base(),
+			value(_value_initial),
+			value_initial(_value_initial),
+			points(points_num),
+			points_index_current(0)
+		{
+			channels_input_num_set(0);
+			channels_output_num_set(1);
+		};
 
 		virtual void perform(sample_buffer& buffer, types::disc_32_u block_size_leq, types::channel offset_channel = 0, types::index offset_sample = 0) = 0;
 
-		void point_add(types::cont_64 delay_s, types::sample value);
+		void reset() {
+			value = value_initial;
+			points_index_current = 0;
+		}
+
+		void point_set(types::disc_32_u point_index, types::time length_s, types::sample value) {
+			if (point_index >= points_num) {
+				throw exceptions::runtime("cdsp::primitives::envelopes::base::point_set: specified point_index out of bounds");
+			}
+			points.at(point_index) = std::make_pair(length_s, value);
+		};
 
 	protected:
 		types::sample value;
---
+++ test/test_interpolate_linear.cpp
@@ -0,0 +1,69 @@
+#include "../src/cdsp.hpp"
+
+using namespace cdsp;
+
+int main(int argc, char* argv[])
+{
+	// create output buffer
+	types::cont_64 sample_rate = 44100.0;
+	types::index block_size = 1024;
+	types::index output_buffer_length = block_size * 32;
+	sample_buffer output_buffer(2, output_buffer_length);
+	output_buffer.clear();
+
+	// create wavetable
+	types::index table_length = 1024;
+	sample_buffer table(1, table_length);
+	helpers::generators::sine(table_length, table.channel_pointer_write(0));
+
+	// create pm_voice
+	abstraction::pm_voice pm_voice_1;
+	pm_voice_1.table_set(table_length, table.channel_pointer_read(0));
+
+	// create envelope
+	primitives::envelopes::interpolate_linear<4> env;
+
+	// prepare
+	pm_voice_1.prepare(sample_rate, block_size);
+	env.prepare(sample_rate, block_size);
+
+	// set parameters
+	pm_voice_1.f_m_set(50.0f / sample_rate);
+	pm_voice_1.i_set(1.0f / values::two_pi_32);
+	pm_voice_1.f_c_set(440.0f / sample_rate);
+	pm_voice_1.a_set(0.5f);
+
+	// perform
+	types::index samples_completed = 0;
+	types::index samples_remaining = output_buffer_length;
+	while (samples_remaining > 0) {
+		types::index block_size_current = block_size;
+		if (samples_remaining < block_size) {
+			block_size_current = samples_remaining;
+		}
+
+		pm_voice_1.perform(output_buffer, block_size_current, 0, samples_completed);
+
+		samples_completed += block_size_current;
+		samples_remaining -= block_size_current;
+		
+		if (samples_remaining == output_buffer_length / 2) {
+			pm_voice_1.reset();
+			pm_voice_1.f_m_set(100.0f / sample_rate);
+			pm_voice_1.i_set(1.0f / values::two_pi_32);
+			pm_voice_1.f_c_set(440.0f / sample_rate);
+			pm_voice_1.a_set(0.5f);
+		}
+	}
+
+	// resize
+	output_buffer.resize(1, output_buffer_length);
+
+	// save
+	helpers::io::wav_file_save("pm_voice_1_test.wav", sample_rate, 32, output_buffer);
+
+	// release
+	pm_voice_1.release();
+ 
+	return 0;
+}
